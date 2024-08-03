07eec6502ce8fc28c8670a54c98e0459636bccee
Hakan Bilgin <hbi99@hotmail.com>
2024-08-03 11:30:42 +0200

    card animations

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -2,5 +2,16 @@
 let Test = {
 	init(APP) {
 		
+		setTimeout(() => {
+			window.find(`.board`).cssSequence("fan-flop", "transitionend", el => {
+				el.cssSequence("flip-flop", "transitionend", el => {
+
+					setTimeout(() => el.addClass("flip-turn"), 300);
+					setTimeout(() => el.addClass("flip-river"), 1300);
+
+				});
+			});
+		}, 500);
+
 	}
 };
